dcc167f99ae1ad58f6f05b4a53cece77943a673f
misael santos <misael.300@hotmail.com>
2019-02-19 22:22:08 -0300

    movimento e rotação do personagem corrigidos

--- Estrategy training/Assets/Scripts/AndroidProject/CharaterMoviments/BasicsMoviments.cs
+++ Estrategy training/Assets/Scripts/AndroidProject/CharaterMoviments/BasicsMoviments.cs
@@ -6,21 +6,26 @@ public class BasicsMoviments : MonoBehaviour
 {
     [SerializeField]
     private Transform characher;
-    private Vector3 fingerPosition;
+
     private Vector2 touch;
-    Vector3 directionToLook;
-    // Start is called before the first frame update
+    private Vector3 directionToLook, fingerPosition, whereToMove;
+    public float speed = 50f, currentDistance, previousDistace;
+    bool isMovement = false;
+
+    Rigidbody rb;
+
     void Start()
     {
         characher = this.transform;
         fingerPosition = new Vector3(0, 0, 0);
         touch = new Vector2(0, 0);
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        if (Input.touchCount > 0 && (Input.GetTouch(0).phase == TouchPhase.Began || Input.GetTouch(0).phase == TouchPhase.Ended))
         {
 
             #region GetTouchPositionToVector3
@@ -33,22 +38,40 @@ public class BasicsMoviments : MonoBehaviour
             if (plane.Raycast(ray, out distance))
             { // if plane hit...
                 fingerPosition = ray.GetPoint(distance); // get the point
-                                                         // pos has the position in the plane you've touched
-
+                                                         // pos has the position in the plane you've touche
                 #endregion
-
                 #region LookAt
                 directionToLook = fingerPosition - characher.position;
                 transform.rotation = Quaternion.LookRotation(directionToLook);
-                #endregion
-                print("target: "+ directionToLook.normalized + "  position: "+ transform.position);
-
-                do
-                {
-                    directionToLook = fingerPosition - transform.position;
-                    transform.position += directionToLook.normalized * 0.1f ; 
-                } while (directionToLook.sqrMagnitude > 0.1f);
+                #endregion   
             }
+            // ActionCallBack(fingerPosition);
+
+
+            if (isMovement)
+                currentDistance = (fingerPosition - transform.position).magnitude;
+
+
+            previousDistace = 0f;
+            currentDistance = 0f;
+            isMovement = true;
+            whereToMove = (fingerPosition - transform.position).normalized;
+            rb.velocity = new Vector3(whereToMove.x * speed, whereToMove.y * speed, whereToMove.z * speed);
+
+
+
+            if (isMovement)
+                previousDistace = (fingerPosition - transform.position).magnitude;
+            if (transform.position == fingerPosition)
+            {
+                print("ta igual");
+                isMovement = false;
+                rb.velocity = Vector3.zero;
+            }
+            print(string.Format("{0}   {1}", transform.position, fingerPosition));
+
         }
+
+
     }
 }
---
+++ Estrategy training/Assets/Scripts/AndroidProject/CharaterMoviments/MovimentsCharacter.cs
@@ -0,0 +1,89 @@
+﻿/*/
+* Script by Devin Curry
+* www.Devination.com
+* www.youtube.com/user/curryboy001
+* Please like and subscribe if you found my tutorials helpful :D
+* Twitter: https://twitter.com/Devination3D
+/*/
+using UnityEngine;
+using System.Collections;
+
+public class MovimentsCharacter : TouchLogicV2//NOTE: This script has been updated to V2 after video recording
+{
+    public float speed = 1f, maxDist = 2;
+    private Vector3 finger, directionToLook;
+    private Transform myTrans, camTrans;
+
+    void Start()
+    {
+        myTrans = this.transform;
+        camTrans = Camera.main.transform;
+    }
+
+    ////separated to own function so it can be called from multiple functions
+    void LookAtFinger()
+    {
+        #region GetTouchPositionToVector3
+        // create ray from the camera and passing through the touch position:
+        if (Input.touchCount > 0) { 
+        Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+        // create a logical plane at this object's position
+        // and perpendicular to world Y:
+        Plane plane = new Plane(Vector3.up, transform.position);
+        float distance = 0; // this will return the distance from the camera
+        if (plane.Raycast(ray, out distance))
+        { // if plane hit...
+            finger = ray.GetPoint(distance); // get the point
+                                             // pos has the position in the plane you've touche
+            #endregion
+            #region LookAt
+            directionToLook = finger - myTrans.position;
+            transform.rotation = Quaternion.LookRotation(directionToLook);
+            #endregion
+        }
+    }
+    //move towards finger if not too close
+    Move(finger, myTrans);
+}
+public void Move(Vector3 finger, Transform characher)
+{
+
+    if (Vector3.Distance(finger, characher.position) > maxDist)
+        characher.Translate(Vector3.forward * speed * Time.deltaTime);
+}
+public Vector2 startPos;
+public Vector2 direction;
+public bool directionChosen;
+void Update()
+{
+    // Track a single touch as a direction control.
+    if (Input.touchCount > 0)
+    {
+        Touch touch = Input.GetTouch(0);
+
+        // Handle finger movements based on touch phase.
+        switch (touch.phase)
+        {
+            // Record initial touch position.
+            case TouchPhase.Began:
+                startPos = touch.position;
+                directionChosen = false;
+                break;
+
+            // Determine direction by comparing the current touch position with the initial one.
+            case TouchPhase.Moved:
+                direction = touch.position - startPos;
+                break;
+
+            // Report that a direction has been chosen when the finger is lifted.
+            case TouchPhase.Ended:
+                directionChosen = true;
+                break;
+        }
+    }
+    if (directionChosen)
+    {
+        LookAtFinger();
+    }
+}
+}

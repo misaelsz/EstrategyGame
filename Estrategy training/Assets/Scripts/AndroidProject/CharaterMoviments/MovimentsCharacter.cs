b0a9f63ba681a63ffb4b4551377ee3dfc58186c8
misael santos <misael.300@hotmail.com>
2019-03-20 21:04:17 -0300

    movimentação ok

--- Estrategy training/Assets/Scripts/AndroidProject/CharaterMoviments/MovimentsCharacter.cs
+++ Estrategy training/Assets/Scripts/AndroidProject/CharaterMoviments/MovimentsCharacter.cs
@@ -1,13 +1,12 @@
 ﻿using UnityEngine;
-using System.Collections;
+using Assets.Scripts.AndroidProject.Tools;
 
 public class MovimentsCharacter : MonoBehaviour
 {
     public float speed = 1f, maxDist = 2;
-    private Vector3 finger, directionToLook;
+    private Vector3 finger, directionToLook,newPos, oldPos;
     private Transform myTrans, camTrans;
-    public Vector2 startPos, direction;
-    public bool directionChosen, insideButton;
+    public bool directionChosen;
     void Start()
     {
         myTrans = this.transform;
@@ -20,77 +19,52 @@ public class MovimentsCharacter : MonoBehaviour
     {
         if (Input.touchCount > 0)
         {
-            
-            Vector2 positionFinger = Camera.main.WorldToScreenPoint(Input.GetTouch(0).position);
-            
             // Track a single touch as a direction control.
             if (Input.touchCount == 1)
             {
                 Touch touch = Input.GetTouch(0);
                 if (touch.position.x < 350f && touch.position.y < 350f)
                 {
-                    insideButton = true;
+                    if (oldPos != myTrans.position)
+                    {
+                        LookAtFinger(oldPos, myTrans);
+                        Move(oldPos, myTrans);
+                    }
                 }
                 else
                 {
-                    insideButton = false;
                     // Handle finger movements based on touch phase.
                     switch (touch.phase)
                     {
                         // Record initial touch position.
                         case TouchPhase.Began:
-                            startPos = touch.position;
+                            newPos = Utils.ConvertToVector3(touch.position, myTrans);
                             directionChosen = false;
                             break;
 
                         // Report that a direction has been chosen when the finger is lifted.
                         case TouchPhase.Ended:
+                            oldPos = newPos;
                             directionChosen = true;
                             break;
                     }
                 }
             }
-
         }
-        if (directionChosen && !insideButton)
+        if (directionChosen)
         {
-            LookAtFinger();
+            LookAtFinger(newPos, myTrans);
+            Move(newPos, myTrans);
         }
-
-
     }
     public void Move(Vector3 finger, Transform characher)
     {
-
         if (Vector3.Distance(finger, characher.position) > maxDist)
             characher.Translate(Vector3.forward * speed * Time.deltaTime);
     }
-    void LookAtFinger()
+    void LookAtFinger(Vector3 finger, Transform myTransform)
     {
-        #region GetTouchPositionToVector3
-        
-        if (Input.touchCount > 0)
-        {
-            Vector2 positionFinger = Camera.main.WorldToScreenPoint(Input.GetTouch(0).position);
-
-            Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
-            // create a logical plane at this object's position
-            // and perpendicular to world Y:
-            Plane plane = new Plane(Vector3.up, transform.position);
-            float distance = 0; // this will return the distance from the camera
-            if (plane.Raycast(ray, out distance))
-            { // if plane hit...
-                finger = ray.GetPoint(distance); // get the point
-                                                 // pos has the position in the plane you've touche
-                #endregion
-                #region LookAt
-                directionToLook = finger - myTrans.position;
-                transform.rotation = Quaternion.LookRotation(directionToLook);
-                #endregion
-            }
-
-        }
-        //move towards finger if not too close
-        Move(finger, myTrans);
+        directionToLook = finger - myTransform.position;
+        transform.rotation = Quaternion.LookRotation(directionToLook);
     }
 }
---
+++ Estrategy training/Assets/Scripts/AndroidProject/Tools/Utils.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.AndroidProject.Tools
+{
+   public static class Utils
+    {
+        public static Vector3 ConvertToVector3(this Vector2 newPosition, Transform characher)
+        {
+            Vector2 positionFinger = Camera.main.WorldToScreenPoint(newPosition);
+            Vector3 finger = new Vector3();
+            Ray ray = Camera.main.ScreenPointToRay(newPosition);
+            // create a logical plane at this object's position
+            // and perpendicular to world Y:
+            Plane plane = new Plane(Vector3.up, characher.position);
+            float distance = 0; // this will return the distance from the camera
+            if (plane.Raycast(ray, out distance))
+            { // if plane hit...
+                finger = ray.GetPoint(distance); // get the point
+            }                               // pos has the position in the plane you've touche
+            return finger;
+        }
+    }
+}
